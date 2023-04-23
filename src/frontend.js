f75c547a64772fce57251c79402381f401835d7b
Richart_Griffin <richard.griffin.artist@gmail.com>
2023-04-23 10:31:37 -0400

    operacion = y preparado para compilar

--- src/frontend.js
+++ src/frontend.js
@@ -1,23 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const pantalla = document.querySelector('.pantalla');
-    const botones = document.querySelectorAll('.botones button');
-  
-    botones.forEach((boton) => {
-      boton.addEventListener('click', () => {
-        const contenidoBoton = boton.textContent;
-  
-        // Si el botón es igual a "=", realiza la operación
-        if (contenidoBoton === '=') {
-          try {
-            const resultado = eval(pantalla.value);
-            pantalla.value = resultado;
-          } catch (error) {
-            pantalla.value = 'Error';
-          }
+  const pantalla = document.querySelector('.pantalla');
+  const botones = document.querySelectorAll('.botones button');
+  let resetPantalla = false;
+
+  const esOperador = (caracter) => {
+    return caracter === '+' || caracter === '-' || caracter === '*' || caracter === '/';
+  };
+
+  botones.forEach((boton) => {
+    boton.addEventListener('click', () => {
+      const contenidoBoton = boton.textContent;
+
+      if (contenidoBoton === '=') {
+        try {
+          const resultado = eval(pantalla.value);
+          pantalla.value = resultado;
+          resetPantalla = true;
+        } catch (error) {
+          pantalla.value = 'Error';
+          resetPantalla = true;
+        }
+      } else {
+        if (resetPantalla) {
+          pantalla.value = '';
+          resetPantalla = false;
+        }
+
+        if (contenidoBoton === 'C') {
+          pantalla.value = '';
+        } else if (contenidoBoton === '←') {
+          pantalla.value = pantalla.value.slice(0, -1);
         } else {
-          // Si no es "=", escribe el contenido del botón en la pantalla
+          if (esOperador(contenidoBoton) && esOperador(pantalla.value.slice(-1))) {
+            pantalla.value = pantalla.value.slice(0, -1);
+          }
           pantalla.value += contenidoBoton;
         }
-      });
+      }
     });
   });
+});
